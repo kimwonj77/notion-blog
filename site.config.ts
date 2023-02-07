1f5b1582a42f0bb1738c4fd646aa755e11e347d9
WonJung Kim <95566897+kimwonj77@users.noreply.github.com>
2023-02-07 11:29:45 +0900

    remove github share button

--- site.config.ts
+++ site.config.ts
@@ -19,7 +19,7 @@ export default siteConfig({
   // social usernames (optional)
   // twitter: 'transitive_bs',
   github: 'kimwonj77',
-  linkedin: 'kimwonj77',
+  linkedin: 'wonjungkim',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
